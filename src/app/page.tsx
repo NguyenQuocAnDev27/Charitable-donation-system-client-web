c70f8a15e151390af2204b5a8a2db43ba9ab4ea2
Nguyễn Quốc An <91923556+NguyenQuocAnDev27@users.noreply.github.com>
2024-11-28 00:22:49 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
 import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
-import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import ImageCarousel from "@/components/ImageCarousel";
-import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonials";
-import Video from "@/components/Video";
 import { Metadata } from "next";
 
-import localFont from 'next/font/local';
 
 export const metadata: Metadata = {
   title: "SGU charity",
